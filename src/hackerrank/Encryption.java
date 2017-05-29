70a5c278317c3f3878be0e7f739b0cfcdadb79dd
jaypatel <jjpatel361@gmail.com>
2017-05-29 12:57:27 -0400

    UPDATE: Encryption without array usage

--- src/hackerrank/Encryption.java
+++ src/hackerrank/Encryption.java
@@ -18,7 +18,6 @@ public class Encryption {
 		String data = sc.nextLine();
 		sc.close();
 		int L = data.length();
-		//int rows = (int) Math.floor(Math.sqrt(L));
 		int cols = (int) Math.ceil(Math.sqrt(L));
 		int skipstep = cols;
 		char[] chardata = data.toCharArray();
@@ -30,48 +29,6 @@ public class Encryption {
 			System.out.print(tmp + " ");
 		}
 		
+		System.exit(0);
 	}
-	
-	
-	
-	
-	public static void main1(String[] args) throws FileNotFoundException {
-		Scanner sc = new Scanner(new File("./data/encryption"));
-		String data = sc.nextLine();
-		//System.out.println(data);
-		sc.close();
-		
-		int L = data.length();
-		int rows = (int) Math.floor(Math.sqrt(L));
-		int cols = (int) Math.ceil(Math.sqrt(L));
-		
-		char[] chardata = data.toCharArray();
-		char[][] master = new char[cols][cols];
-		
-		int k = 0;
-		
-		for (int i = 0; i < cols; i++) {
-			for (int j = 0; j < cols; j++) {
-				if(k > chardata.length -1){
-					break;
-				}
-				master[i][j] = chardata[k];
-				k++;
-			}
-		}
-				
-		String encrypted_result = "";
-		for (int i = 0; i < cols; i++) {
-			for (int j = 0; j < cols; j++) {
-				encrypted_result += master[j][i];
-			}
-			encrypted_result += " ";
-		}
-		
-		
-		System.out.print(encrypted_result.trim());
-		
-		
-	}
-
 }
