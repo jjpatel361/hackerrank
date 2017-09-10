80f815c3ee61b6936648250de8e694ef76efcd37
jaypatel <jjpatel361@gmail.com>
2017-09-10 11:32:24 -0400

    LC: Two Sum Solved.

--- src/leetcode/TwoSum.java
+++ src/leetcode/TwoSum.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+
 import java.util.Arrays;
 
 public class TwoSum {
@@ -9,83 +10,53 @@ public class TwoSum {
 	 * 
 	 * */
 	public static void main(String[] args) {
-		//int[] p = new int[] {2, 7, 11, 15}; // 9
-		//int[] p = new int[] {3,2,4}; // 6
-		//int[] p = new int[]{3,3}; // 6
-	    //int [] p = new int[]{-3,4,3,90}; // 0
 		
-		int [] p = new int[]{-1,-2,-3,-4,-5}; // - 8 
+		int [] p = new int[]{-1,-2,-3,-4,-5}; // - 8  // [2, 4] 
 		
 		int target = -8;
 		
 		int[] result = twoSum(p, target);
 		for (int i : result) {
-			System.out.print(i);
+			System.out.print(i + " ");
 		}
 	}
 	
 	
 	public static int[] twoSum(int[] nums, int target) {
 		
-		int[] sorted_data = nums.clone();
+		int[] sorted_arr = nums.clone();
+		int[] result = new int[2];
 		
-		Arrays.sort(sorted_data);
+		Arrays.sort(sorted_arr);
 		
-		// search index where 
-		// value exceeds/equal target
-		int index = 0;
-		
-		for (int i = 0; i < sorted_data.length; i++) {
-			index = i;
-		
-			if(sorted_data[i] > target) {
-				break;
-			}
-		}
-		
-		int i = 0; // starting point
-		int j = index; // j points to index till we want the searching to happen
-		int[] result_index = new int[2];
-		int found = 0;
-		
-		while(i < j) {
+		for (int i = 0; i < sorted_arr.length - 1; i++) {
 			
-			int smaller_number = sorted_data[i];
-			int larger_number = sorted_data[j];
-			int sum = smaller_number + larger_number;
-			
-			if(sum == target) {
-				// search for smaller number
-				for (int k = 0; k < nums.length; k++) {
-					if(smaller_number == nums[k]) {
-						result_index[0] = k;
-						found++;
+			int first_number = sorted_arr[i];
+			int second_number = target - first_number;
+			int[] search_window = Arrays.copyOfRange(sorted_arr, i + 1, sorted_arr.length);
+			int index = Arrays.binarySearch(search_window, second_number);
+			if(index >= 0) {
+				// look up values 
+				for (int j = 0; j < nums.length; j++) {
+					if(nums[j] == first_number) {
+						result[0] = j;
 						break;
 					}
 				}
 				
-				for (int k = 0; k < nums.length; k++) {
-					if(larger_number == nums[k] && k!= result_index[0]) {
-						result_index[1] = k;
-						found++; // remove found not used
+				// look up for second index
+				for (int j = 0; j < nums.length; j++) {
+					if(nums[j] == second_number && j != result[0]) {
+						result[1] = j;
 						break;
 					}
-				}
-				
-				break; 
-				
+				} 
+				 break;
 			}
 			
-			if(sum > target) {
-				j--;
-			}
-			
-			if(sum < target) {
-				i++;
-			}
 			
 		}
-		return result_index;
-    }
+		return result;
+	}
 
 }
