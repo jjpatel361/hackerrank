827a9286102fa95c17104fe9163debe55239f6fa
jjpatel361 <jjpatel361@gmail.com>
2018-05-30 21:19:41 -0400

    New TreeNode Added

---
+++ src/models/TreeLinkNode.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package models;
+
+/**
+ * @author Jay Patel
+ *
+ */
+public class TreeLinkNode {
+	public Integer val;
+	public TreeLinkNode left;
+	public TreeLinkNode right;
+	public TreeLinkNode next;
+	public TreeLinkNode(int x) { val = x; }
+
+}
